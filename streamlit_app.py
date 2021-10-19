a1810233efdda329d70876e9a5f18c048ba01289
manolinux <mcotallo@gmail.com>
2021-10-19 11:40:11 +0200

    Improvements in communication map/form

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ from fastapi.encoders import jsonable_encoder
 import tempfile
 import geojson
 from settings import Settings
+from communication import communication
+
 
 ##Session state
 def setSessionState(key,value):
@@ -24,6 +26,12 @@ def stContainerText(container,text):
     container.empty()
     with container:
         st.text(text)
+
+##Recreate a text input in container
+def stContainerTextInput(container,label,value):
+    container.empty()
+    with container:
+         return st.text_input(label=label,value=value)
 
 #process turnovers into groups by age and male/female
 def processTurnovers(tdata,postalCode):
@@ -46,30 +54,45 @@ def getGeometryFromPropertyAndValue(geojson,prop,val):
                 return ft['geometry']
     #Non exisitng prop
     except Exception as e:
-        st.error(str(e))
+        st.error(str(prop)+str(val))
     return None
 
 #get Geometry centroid
 def getGeometryCentroid(geom):
     return shape(geom).centroid
 
+#def changed postal code
+def changedPostalCode(args):
+    st.write("postal code = " + args)
+
+#Page config wide
 st.set_page_config(layout="wide")
 
 #Custom html for keeping some things
 #postalCodes in advance
 postalCodesData = requests.get('http://localhost:8000/postalCodes/4326').json()
-st.session_state['postalCodes'] = postalCodesData
-st.session_state['lon'] = -3.703889
-st.session_state['lat'] = 40.416667
-st.session_state['startDate'] = datetime.strptime('2015-01-01','%Y-%M-%d')
-st.session_state['endDate'] = datetime.strptime('2017-12-31','%Y-%M-%d')
-st.session_state['zipcode'] = '28013'
-st.session_state['turnovers'] = None
-st.session_state['turnoversByAgeAndGender'] = None
+if 'postalCodes' not in st.session_state:
+    st.session_state['postalCodes'] = postalCodesData
+if 'lon' not in st.session_state:
+    st.session_state['lon'] = -3.703889
+if 'lat' not in st.session_state:
+    st.session_state['lat'] = 40.416667
+if 'startDate' not in st.session_state:
+    st.session_state['startDate'] = datetime.strptime('2015-01-01','%Y-%M-%d')
+if 'endDate' not in st.session_state:
+    st.session_state['endDate'] = datetime.strptime('2017-12-31','%Y-%M-%d')
+if 'zipcode' not in st.session_state:
+    st.session_state['zipcode'] = '28013'
+if 'turnovers' not in st.session_state:
+    st.session_state['turnovers'] = None
+if 'turnoversByAgeAndGener' not in st.session_state:
+    st.session_state['turnoversByAgeAndGender'] = None
 
 col1, col2,col3 = st.columns((1,2,1))
 with col1:
+    sCode = communication()
     with st.form("Form 1"): 
+      
         d1 = st.date_input ("Start date" , max_value=datetime.strptime('2017-12-31','%Y-%M-%d'), 
                 min_value=datetime.strptime('2015-01-01','%Y-%M-%d'), 
                 value=st.session_state["startDate"])
@@ -77,8 +100,8 @@ with col1:
                 max_value=datetime.strptime('2025-12-31','%Y-%M-%d'),
                 value=st.session_state["endDate"])
                 
-
-        cp = st.text_input("Selected zip code",value=st.session_state["zipcode"])
+        cpContainer = st.empty()
+        cp = stContainerTextInput(cpContainer,label="Selected zip code",value=st.session_state["zipcode"])
         b1 = st.form_submit_button("Go!")
         acumHeader = st.subheader('Acummulated')
         periodContainer = st.empty()
@@ -86,15 +109,20 @@ with col1:
         stContainerText(periodContainer,'Period: N/A')
         stContainerText(valueContainer,'Value: N/A')
     
+        if sCode:
+            st.session_state["zipcode"] = sCode["value"]
+            del cp
+            cp = stContainerTextInput(cpContainer,label="Selected zip code",value=st.session_state["zipcode"])
+
         if d1 or d2:
             stContainerText(periodContainer,'Period: ' + datetime.strftime(d1,'%Y/%m/%d') + " - " + 
                         datetime.strftime(d2,'%Y/%m/%d'))
             stContainerText(valueContainer,'Value: N/A')
             setSessionState("startDate", d1)
             setSessionState("endDate", d2)
-        if cp:
-            setSessionState("zipcode",cp)
-  
+        
+            
+           
         #Button clicked?
         if b1:
             try:
@@ -138,7 +166,7 @@ with col2:
         "fillColor": "#00ff00",
         "fillOpacity": 0.1,
     }
-   
+    
     if cp:
 
         DEFAULT_MAP_SPECS = dict(height=450, width=600) 
@@ -191,19 +219,57 @@ with col2:
         #Add postal codes layer
         m.add_geojson(postalCodesData,layer_name="Postal codes",
             style=style_1,hover_style=hover_style)
-
-
                             
+    aditionalJavascript = """
+        <script>
+            Streamlit = top.Streamlit;
+            var __assign = (this && this.__assign) || function () {
+                __assign = Object.assign || function(t) {
+                    for (var s, i = 1, n = arguments.length; i < n; i++) {
+                        s = arguments[i];
+                        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                            t[p] = s[p];
+                    }
+                return t;
+                };
+                return __assign.apply(this, arguments);
+            };
+          
+            window.store.subscribe(function() {
+                var map = window.store.getState().keplerGl.map;
+                if (typeof map.visState.clicked != 'undefined') {
+                    var object = map.visState.clicked.object;
+                    if (typeof object != 'undefined' && typeof object.properties != 'undefined') 
+                    {
+                           Streamlit.sendBackMsg("streamlit:setComponentValue", {
+                                value: { value: map.visState.clicked.object.properties.code},
+                                dataType: "json"
+                            });
+                        
+                    }
+                
+                }
+            });
+        </script>
+    """
+    
+    html = m.to_html()
+    html = html.replace("Object(n.createStore)","window.store=Object(n.createStore)",1)
+    closeBodyPos = html.find("</body>")
+    
+    #Insert aditional javascript before closing </body>
     components.html(
-        m.to_html(),
+        f"{html[:closeBodyPos]}{aditionalJavascript}{html[closeBodyPos:]}",
         width=map_specs['width'],
         height=map_specs['height'],
         scrolling=False)
+
     
     #m.to_streamlit()
     
     
 with col3:
+    
     st.write("Turnovers by age and gender",allow_unsafe_html=True)
     valuesForChart1 = st.session_state['turnoversByAgeAndGender'].properties
     pdf1 = pd.DataFrame([k for k in valuesForChart1.values()],
